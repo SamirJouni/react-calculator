9a24bc3f276ea6d1c3fec6c15ce1d11b919f2315
F1r3byte <f1r3byte@yandex.com>
2019-05-18 14:13:11 -1200

    added regex

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
 import "./Input.css";
 
 const Input = props => {
 	return (
 		<div>
-			<input type="text" name="input" value={props.input} className="input" onChange={(e) => props.onInput(e.target.value, true)}/>
+			<input
+				type="text"
+				name="input"
+				value={props.input}
+				className="input"
+				onChange={e => props.onInput(e.target.value, true)}
+			/>
 		</div>
-	 );
-}
+	);
+};
 
 export default Input;
